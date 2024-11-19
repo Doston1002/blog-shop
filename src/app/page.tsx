86d57ef65932142592b157fe1db563896f1808ec
Doston1002 <polatovdoston1002@gmail.com>
2024-11-19 13:46:44 +0500

    code update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 			<Hero />
 			<section className='flex flex-col space-y-12'>
 				<h1 className='text-5xl font-bold text-center'>
-					SAMMI SHOP DEALS
+					CHEGIRMA SHOP DEALS
 				</h1>
 				<div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
 					{products.map(product => (
